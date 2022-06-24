bb30fdcfd0e0a0b033af54e3c9d55d76e480925f
Daphine <105224931+l-hjab@users.noreply.github.com>
2022-06-24 11:35:33 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+let name="Daphine"
+let country="Kenya"
+let tallness="1.3feet"
+
+let result="\r\nName: " + name
+let Country="\r\nCountry: "+ country
+let height="\r\nHeight: "+ tallness
+console.log(result);
+console.log(Country);
+console.log(height)
